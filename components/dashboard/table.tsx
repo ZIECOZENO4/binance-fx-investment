1540d45868388db556d6ea61a7ae6e6529ad2f52
FirsT2015 <you@example.com>
2024-02-04 11:28:22 +0100

    vnmcxm

--- components/dashboard/table.tsx
+++ components/dashboard/table.tsx
@@ -1,37 +1,42 @@
 'use client'
 import React, { useState } from 'react';
+import { motion } from 'framer-motion';
+import Watchlist from './watchlist';
+import Market from './market';
+import Assets from './assets';
 
-const Table = () => {
- const [selectedItem, setSelectedItem] = useState('assets');
-
- const renderComponent = async () => {
-    let Component;
-
-    switch(selectedItem) {
-      case 'assets':
-        Component = await import('./assets');
-        break;
-      case 'watchlist':
-        Component = await import('./watchlist');
-        break;
-      case 'market':
-        Component = await import('./market');
-        break;
-      default:
-        return null;
-    }
-
-    return <Component.default />;
- };
+function Table() {
+ const [activeTab, setActiveTab] = useState('Assets');
 
  return (
-    <div>
-      <div onClick={() => setSelectedItem('assets')} className={selectedItem === 'assets' ? 'active' : ''}>Car</div>
-      <div onClick={() => setSelectedItem('watchlist')} className={selectedItem === 'watchlist' ? 'active' : ''}>Ball</div>
-      <div onClick={() => setSelectedItem('market')} className={selectedItem === 'market' ? 'active' : ''}>Nes</div>
-      {renderComponent()}
-    </div>
+ <div>
+   <div className='flex flex-row mx-2 justify-between'>
+     <motion.header
+       onClick={() => setActiveTab('Assets')}
+       className={`text-slate-200 font-heading  text-2xl border-b-2 mx-5 pt-4  cursor-pointer ${activeTab === 'Assets' ? 'border-green-700  rounded-md  bg-gradient-to-r from-red-500 to-sky-500 bg-clip-text text-transparent animate-gradient' : 'border-amber-100'}`}
+       animate={{ scale: activeTab === 'Assets' ? 1.2 : 1 }}
+     >
+    Assets
+     </motion.header>
+     <motion.header
+       onClick={() => setActiveTab('Market')}
+       className={`text-slate-200 font-heading  text-2xl border-b-2 mx-5 pt-4  cursor-pointer ${activeTab === 'Market' ? 'border-green-700  rounded-md  bg-gradient-to-r from-red-500 to-sky-500 bg-clip-text text-transparent animate-gradient' : 'border-amber-100'}`}
+     >
+    Market
+     </motion.header>
+     <motion.header
+       onClick={() => setActiveTab('Watchlist')}
+       className={`text-slate-200 font-heading  text-2xl border-b-2 mx-5 pt-4  cursor-pointer ${activeTab === 'Watchlist' ? 'border-green-700  rounded-md  bg-gradient-to-r from-red-500 to-sky-500 bg-clip-text text-transparent animate-gradient' : 'border-amber-100'}`}
+     >
+     Watchlist
+     </motion.header>
+   </div>
+   <div  className=' h-1 w-full bg-slate-300 mt-2 mb-5'/>
+   {activeTab === 'Assets' && <div><Assets /></div>}
+   {activeTab === 'Market' && <div><Market /></div>}
+   {activeTab === 'Watchlist' && <div><Watchlist /></div>}
+ </div>
  );
-};
+}
 
 export default Table;
