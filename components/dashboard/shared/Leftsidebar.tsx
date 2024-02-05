452361adc97c096513db92f6afce2ba9d3ca9ceb
FirsT2015 <you@example.com>
2024-02-05 16:29:38 +0100

    po

--- components/dashboard/shared/Leftsidebar.tsx
+++ components/dashboard/shared/Leftsidebar.tsx
@@ -6,49 +6,29 @@ import React from 'react'
 import Link from 'next/link'
 import { usePathname, useRouter} from 'next/navigation';
 function LeftSidebar() {
-const router = useRouter();
-const pathname = usePathname();
+ const router = useRouter();
+ const pathname = usePathname();
 
+ return (
+    <section className='sticky left-0 top-0 z-20 flex h-screen w-fit flex-col justify-between overflow-auto border-r border-r-dark-4 bg-dark-2 pb-5 pt-6 max-md:hidden'>
+      <div className='flex w-full flex-1 flex-col gap-4 px-6'>
+        {
+          sidebarLinks.map((link) => {
+            // Check if the current pathname exactly matches the link's route
+            const isActive = pathname === link.route;
 
-  return (
-    <section  className='  sticky left-0 top-0 z-20 flex h-screen w-fit flex-col justify-between overflow-auto border-r border-r-dark-4 bg-dark-2 pb-5 pt-6 max-md:hidden'>
-      <div className=' flex w-full flex-1 flex-col gap-4 px-6'>
-      {
-        sidebarLinks.map ((link) =>{
-        const isActive = (pathname.includes(link.route) && link.route.length > 1) || pathname === link.route;
-
-        
-        return(
-         <Link href={link.route} key={link.label} className={`relative flex justify-start gap-4 rounded-lg p-4 ${isActive && ' bg-[#FFD700]'}`}>
-          <Image src={link.imgURL} alt={link.label} width={28} height={28}/>
-          <p className=' text-light-1 font-bold  max-lg:first-letter:hidden'>{link.label}</p>
-         </Link> 
-        )
-        } )
-      }
-    </div>   
-    <div className=' mt-10 px-6'>
-   
-    </div>
+            return (
+              <Link href={link.route} key={link.label} className={`relative flex justify-start gap-4 rounded-lg p-4 ${isActive ? 'bg-[#FFD700]' : ''}`}>
+                <Image src={link.imgURL} alt={link.label} width={28} height={28} />
+                <p className='text-light-1 font-bold max-lg:first-letter:hidden'>{link.label}</p>
+              </Link>
+            );
+          })
+        }
+      </div>
+      <div className='mt-10 px-6'></div>
     </section>
- 
-  )
+ );
 }
 
-export default LeftSidebar
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+export default LeftSidebar;
