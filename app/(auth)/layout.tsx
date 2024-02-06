8cba0349f2871334e3ba07a7acad3b710611c835
FirsT2015 <you@example.com>
2024-02-06 01:03:50 +0100

    nhg

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -18,15 +18,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-
-    >
-      <html lang='en'>
         <body className={`${inter.className} bg-dark-1`}>
           <div className=" w-full flex justify-center items-center min-h-screen">
             {children}   
           </div>
        </body>
-      </html>
-
   );
 }
