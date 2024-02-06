1c37e697dd92eeacdf70f850cffaed8c8daec6f3
FirsT2015 <you@example.com>
2024-02-06 20:37:37 +0100

    nhg

--- components/invest/invest.tsx
+++ components/invest/invest.tsx
@@ -1,31 +1,44 @@
-'use client'
+"use client"
 import { useQuery } from 'react-query';
 import axios from 'axios';
+import { FC } from 'react';
 
-async function fetchTrendingCoins() {
-  const response = await axios.get('https://api.coingecko.com/api/v3/search/trending');
+interface TrendingCoin {
+  id: string;
+  name: string;
+}
+
+interface TrendingCoinsResponse {
+  coins: TrendingCoin[];
+}
+
+async function fetchTrendingCoins(): Promise<TrendingCoinsResponse> {
+  const response = await axios.get<TrendingCoinsResponse>('https://api.coingecko.com/api/v3/search/trending');
   return response.data;
 }
 
-export default function Invest() {
-  const { isLoading, isError, data, error } = useQuery('trendingCoins', fetchTrendingCoins);
+const Invest: FC = () => {
+  const { isLoading, isError, data, error } = useQuery<TrendingCoinsResponse, Error>('trendingCoins', fetchTrendingCoins);
 
   if (isLoading) {
     return <div>Loading...</div>;
   }
 
   if (isError) {
-    return <div>Error: {error.message}</div>;
+    const errorMessage = error instanceof Error ? error.message : 'An unknown error occurred';
+    return <div>Error: {errorMessage}</div>;
   }
 
   return (
     <div>
       <h1>Trending Coins</h1>
       <ul>
-        {data.coins.map((coin) => (
+        {data?.coins.map((coin) => (
           <li key={coin.id}>{coin.name}</li>
         ))}
       </ul>
     </div>
   );
-}
+};
+
+export default Invest;
