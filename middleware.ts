eedf889866ad10b2e038b413b613ac57ba5fce53
FirsT2015 <you@example.com>
2024-02-07 16:09:52 +0100

    bv

--- middleware.ts
+++ middleware.ts
@@ -5,10 +5,10 @@ import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
   // An array of public routes that don't require authentication.
-  publicRoutes: ["/"],
+  publicRoutes: ["/","/dashboard/invest"],
 
   // An array of routes to be ignored by the authentication middleware.
-  ignoredRoutes: ["/"],
+  ignoredRoutes: ["/","/dashboard/invest"],
 });
 
 export const config = {
