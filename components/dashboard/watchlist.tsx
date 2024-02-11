39229bcaf85837c566a57f3909fbabaed6f3f8d4
FirsT2015 <you@example.com>
2024-02-11 21:57:47 +0100

    bnmnb

--- components/dashboard/watchlist.tsx
+++ components/dashboard/watchlist.tsx
@@ -4,7 +4,7 @@ const Watchlist = () => {
   return (
     <div  className=' text-bold text-white text-2xl flex justify-center align-middle '>
       <div className=" flex flex-row justify-between mx-8">
-     <p>My Watchlist</p>
+     <p className="text-bold">My Watchlist</p>
      <p className="md:hidden">Edit</p>
       </div>
       <div>
