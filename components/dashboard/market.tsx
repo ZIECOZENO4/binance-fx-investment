25e5da933794cc5fa4052dba28a571bedd5fe43d
FirsT2015 <you@example.com>
2024-02-13 12:45:17 +0100

    c;oi

--- components/dashboard/market.tsx
+++ components/dashboard/market.tsx
@@ -405,7 +405,7 @@ export default function Market() {
 
   useEffect(() => {
     const fetchData = async () => {
-      const url1 = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false&locale=en';
+      const url1 = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en';
       const options1 = {
         method: 'GET',
         headers: {
