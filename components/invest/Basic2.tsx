7ec22b3e6137368ad2eae550d2c196891d2dff92
FirsT2015 <you@example.com>
2024-02-14 13:29:27 +0100

    uiygdchj

--- components/invest/Basic2.tsx
+++ components/invest/Basic2.tsx
@@ -10,26 +10,26 @@ const Basic2 = () => {
    <div className='flex flex-row md:mx-2 justify-between pt-14'>
      <motion.header
        onClick={() => setActiveTab('Basic')}
-       className={`text-slate-200 font-heading  text-2xl border-b-2 md:mx-5  mx-1 pt-4  cursor-pointer ${activeTab === 'Basic' ? 'border-green-700  rounded-md  bg-gradient-to-r from-red-500 to-sky-500 bg-clip-text text-transparent animate-gradient' : 'border-amber-100'}`}
+       className={`text-slate-200 font-heading text-xl md:text-2xl border-b-2 md:mx-5  mx-1 pt-4  cursor-pointer ${activeTab === 'Basic' ? 'border-green-700  rounded-md  bg-gradient-to-r from-red-500 to-sky-500 bg-clip-text text-transparent animate-gradient' : 'border-amber-100'}`}
        animate={{ scale: activeTab === 'Basic' ? 1.2 : 1 }}
      >
     Basic
      </motion.header>
      <motion.header
        onClick={() => setActiveTab('Advance')}
-       className={`text-slate-200 font-heading  text-2xl border-b-2 md:mx-5 mx-1 pt-4 backdrop:pt-4  cursor-pointer ${activeTab === 'Advance' ? 'border-green-700  rounded-md  bg-gradient-to-r from-red-500 to-sky-500 bg-clip-text text-transparent animate-gradient' : 'border-amber-100'}`}
+       className={`text-slate-200 font-heading  text-xl md:text-2xl border-b-2 md:mx-5 mx-1 pt-4 backdrop:pt-4  cursor-pointer ${activeTab === 'Advance' ? 'border-green-700  rounded-md  bg-gradient-to-r from-red-500 to-sky-500 bg-clip-text text-transparent animate-gradient' : 'border-amber-100'}`}
      >
     Advance
      </motion.header>
      <motion.header
-       onClick={() => setActiveTab('Pro Plan')}
-       className={`text-slate-200 font-heading  text-2xl border-b-2 md:mx-5 mx-1 pt-4  cursor-pointer ${activeTab === 'Premium' ? 'border-green-700  rounded-md  bg-gradient-to-r from-red-500 to-sky-500 bg-clip-text text-transparent animate-gradient' : 'border-amber-100'}`}
+       onClick={() => setActiveTab('Premium')}
+       className={`text-slate-200 font-heading  text-xl md:text-2xl border-b-2 md:mx-5 mx-1 pt-4  cursor-pointer ${activeTab === 'Premium' ? 'border-green-700  rounded-md  bg-gradient-to-r from-red-500 to-sky-500 bg-clip-text text-transparent animate-gradient' : 'border-amber-100'}`}
      >
      Premium
      </motion.header>
      <motion.header
-       onClick={() => setActiveTab('Premium')}
-       className={`text-slate-200 font-heading  text-2xl border-b-2 md:mx-5 mx-1 pt-4  cursor-pointer ${activeTab === 'Pro Plan' ? 'border-green-700  rounded-md  bg-gradient-to-r from-red-500 to-sky-500 bg-clip-text text-transparent animate-gradient' : 'border-amber-100'}`}
+       onClick={() => setActiveTab('Pro Plan')}
+       className={`text-slate-200 font-heading  text-xl md:text-2xl border-b-2 md:mx-5 mx-1 pt-4  cursor-pointer ${activeTab === 'Pro Plan' ? 'border-green-700  rounded-md  bg-gradient-to-r from-red-500 to-sky-500 bg-clip-text text-transparent animate-gradient' : 'border-amber-100'}`}
      >
      Pro Plan
      </motion.header>
@@ -44,12 +44,48 @@ const Basic2 = () => {
 <p>Max deposit $2,000</p>
 <p>5% referral bonus</p>
 <div>
-  
+
 </div>
   </div>}
-   {activeTab === 'Advance' && <div>yea</div>}
-   {activeTab === 'Pro Plan' && <div>nope</div>}
-   {activeTab === 'Premium' && <div>nopejuytgf</div>}
+   {activeTab === 'Advance' && 
+     <div className=" flex flex-col justify-center align-middle p-2 font-bold font-sono text-xl text-white leading-6 gap-2 md:gap-4">
+    <h1 className=" text-2xl text-sky-600">5% Daily </h1>
+<p>Total roll: 60%</p>
+<p>DURATION: 12 days</p>
+<p>Min deposit $2,100</p>
+<p>Max deposit $10,000</p>
+<p>5% referral bonus</p>
+<div>
+  
+</div>
+  </div>
+   }
+   {activeTab === 'Pro Plan' && 
+     <div className=" flex flex-col justify-center align-middle p-2 font-bold font-sono text-xl text-white leading-6 gap-2 md:gap-4">
+     <h1 className=" text-2xl text-sky-600">5% Daily </h1>
+ <p>Total roll: 80%</p>
+ <p>DURATION: 14 days</p>
+ <p>Min deposit $10,100</p>
+ <p>Max deposit $20,000</p>
+ <p>5% referral bonus</p>
+ <div>
+   
+ </div>
+   </div>
+   }
+   {activeTab === 'Premium' && 
+    <div className=" flex flex-col justify-center align-middle p-2 font-bold font-sono text-xl text-white leading-6 gap-2 md:gap-4">
+    <h1 className=" text-2xl text-sky-600">5% Daily </h1>
+<p>Total roll: 100%</p>
+<p>DURATION: 48 days</p>
+<p>Min deposit $21,100</p>
+<p>Max deposit $50,000</p>
+<p>5% referral bonus</p>
+<div>
+  
+</div>
+  </div>
+   }
  </div>
   )
 }
--- components/invest/Premium2.tsx
+++ components/invest/Premium2.tsx
@@ -1,8 +1,74 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react';
+import { motion } from 'framer-motion';
+
 
 const Premium2 = () => {
+  const [activeTab, setActiveTab] = useState('VIP -1');
   return (
-    <div>Premium2</div>
+    <div>
+   <div className='flex flex-row md:mx-2 justify-between pt-14'>
+     <motion.header
+       onClick={() => setActiveTab('VIP -1')}
+       className={`text-slate-200 font-heading  text-xl md:text-2xl border-b-2 md:mx-5  mx-1 pt-4  cursor-pointer ${activeTab === 'VIP -1' ? 'border-green-700  rounded-md  bg-gradient-to-r from-red-500 to-sky-500 bg-clip-text text-transparent animate-gradient' : 'border-amber-100'}`}
+       animate={{ scale: activeTab === 'VIP -1' ? 1.2 : 1 }}
+     >
+    VIP -1
+     </motion.header>
+     <motion.header
+       onClick={() => setActiveTab('VIP -2')}
+       className={`text-slate-200 font-heading  text-xl md:text-2xl border-b-2 md:mx-5 mx-1 pt-4 backdrop:pt-4  cursor-pointer ${activeTab === 'VIP -2' ? 'border-green-700  rounded-md  bg-gradient-to-r from-red-500 to-sky-500 bg-clip-text text-transparent animate-gradient' : 'border-amber-100'}`}
+     >
+    VIP -2
+     </motion.header>
+     <motion.header
+       onClick={() => setActiveTab('Pro Plan')}
+       className={`text-slate-200 font-heading  text-xl md:text-2xl border-b-2 md:mx-5 mx-1 pt-4  cursor-pointer ${activeTab === 'VIP -3' ? 'border-green-700  rounded-md  bg-gradient-to-r from-red-500 to-sky-500 bg-clip-text text-transparent animate-gradient' : 'border-amber-100'}`}
+     >
+     VIP -3
+     </motion.header>
+
+   </div>
+   <div  className=' h-1 w-full bg-slate-300 mt-2 mb-5'/>
+   {activeTab === 'VIP -1' && 
+   <div className=" flex flex-col justify-center align-middle p-2 font-bold font-sono text-xl text-white leading-6 gap-2 md:gap-4">
+    <h1 className=" text-2xl text-sky-600">75% Daily </h1>
+<p>Total roll: 150%</p>
+<p>DURATION: 48 days</p>
+<p>Min deposit $50,000</p>
+<p>Max deposit $100,000</p>
+<p>5% referral bonus</p>
+<div>
+
+</div>
+  </div>}
+   {activeTab === 'VIP -2' && 
+     <div className=" flex flex-col justify-center align-middle p-2 font-bold font-sono text-xl text-white leading-6 gap-2 md:gap-4">
+    <h1 className=" text-2xl text-sky-600">100% Daily </h1>
+<p>Total roll: 200%</p>
+<p>DURATION: 48 days</p>
+<p>Min deposit $100,000</p>
+<p>Max deposit $200,000</p>
+<p>5% referral bonus</p>
+<div>
+  
+</div>
+  </div>
+   }
+   {activeTab === 'VIP -3' && 
+    <div className=" flex flex-col justify-center align-middle p-2 font-bold font-sono text-xl text-white leading-6 gap-2 md:gap-4">
+    <h1 className=" text-2xl text-sky-600">125% Daily </h1>
+<p>Total roll: 100%</p>
+<p>DURATION: 48 days</p>
+<p>Min deposit $200,000</p>
+<p>Max deposit $500,000</p>
+<p>5% referral bonus</p>
+<div>
+  
+</div>
+  </div>
+   }
+ </div>
   )
 }
 
