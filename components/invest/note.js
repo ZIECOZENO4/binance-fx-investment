817b04d2a32b0b7023f34d128745426909b46951
FirsT2015 <you@example.com>
2024-02-14 11:24:41 +0100

    iurfgh

--- components/invest/note.js
+++ components/invest/note.js
@@ -1,6 +1,6 @@
 'use client';
 import { motion } from "framer-motion";
-import styles from '../../styles';
+import styles from './index';
 import { fadeIn, staggerContainer} from '../../utils/motion'
 import { TypingText } from "../../components/affiliate/content/CustomTexts";
 
