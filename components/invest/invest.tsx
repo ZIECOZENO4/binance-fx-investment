ee73d726de93d254f8068d7b53099454df018771
FirsT2015 <you@example.com>
2024-02-14 12:19:58 +0100

    ouyfgcd

--- components/invest/invest.tsx
+++ components/invest/invest.tsx
@@ -1,6 +1,8 @@
 'use client'
 import { useEffect, useState } from 'react';
 import Note from "./note"
+import { useUser } from "@clerk/clerk-react";
+import { SignedIn, SignedOut, UserButton,  } from "@clerk/nextjs";
 import { motion } from "framer-motion";
 import Basic2 from './Basic2';
 import Premium2 from './Premium2';
@@ -25,6 +27,7 @@ interface CoinDataItem {
 }
 
 const Invest = () => {
+
   const [data1, setData1] = useState<CoinDataItem[] | null>(null);
   const [activeTab, setActiveTab] = useState('BOARD');
   useEffect(() => {
@@ -48,14 +51,32 @@ const Invest = () => {
 
     fetchData();
   }, []);
-
+  const { isLoaded, isSignedIn, user } = useUser();
+  if (!isLoaded) {
+    return null;
+  }
   return (
     <div className='text-white h-[200vh] '>
-    <div className=' relative w-[100vw] h-[50vh] bg-no-repeat object-cover bg-track flex justify-center items-center align-middle  bg-fixed  '>
+    <div className=' relative md:w-[100vw] w-[100vw] md:h-[50vh] h-[20vh] bg-no-repeat object-cover bg-track flex justify-center items-center align-middle  bg-fixed  '>
     </div>
     <Note />
     <div>
-    <div className='flex flex-row px-2 md:px-[20vw] justify-between pt-8 w-[100vw] bg-slate-700'>
+      <div className=" mt-10 md:mt-20 bg-gradient-to-r from-blue-600 to-sky-200 flex-row flex justify-between align-middle items-center">
+<div className=" flex  flex-col align-middle items-center justify-center">
+<svg fill="#1b306f" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 95.633 95.633" stroke="#1b306f"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <path d="M93.632,2.24H2c-1.104,0-2,0.896-2,2v87.152c0,1.104,0.896,2,2,2h91.633c1.104,0,2-0.896,2-2V4.24 C95.632,3.135,94.736,2.24,93.632,2.24z M25.606,10.441c5.44,0,9.85,4.409,9.85,9.849s-4.409,9.851-9.85,9.851 s-9.85-4.41-9.85-9.851C15.756,14.851,20.166,10.441,25.606,10.441z M11.602,36.522c2.038-3.304,7.271-4.883,12.24-5.233 c0.585-0.042,1.175,1.251,1.764,1.423c0.589-0.172,1.179-1.465,1.764-1.423c4.969,0.351,10.202,1.93,12.24,5.233 c0,0,7.055,12.263,1.117,19.046H25.606H10.485C4.547,48.785,11.602,36.522,11.602,36.522z M86.566,82.816 c0,0.828-0.672,1.5-1.5,1.5h-75.5c-0.828,0-1.5-0.672-1.5-1.5v-4.125c0-0.828,0.672-1.5,1.5-1.5h75.5c0.828,0,1.5,0.672,1.5,1.5 V82.816z M86.566,67.816c0,0.828-0.672,1.5-1.5,1.5h-75.5c-0.828,0-1.5-0.672-1.5-1.5v-4.125c0-0.828,0.672-1.5,1.5-1.5h75.5 c0.828,0,1.5,0.672,1.5,1.5V67.816z M86.566,52.816c0,0.828-0.672,1.5-1.5,1.5h-24.5c-0.828,0-1.5-0.672-1.5-1.5v-4.125 c0-0.828,0.672-1.5,1.5-1.5h24.5c0.828,0,1.5,0.672,1.5,1.5V52.816z M86.566,37.816c0,0.828-0.672,1.5-1.5,1.5h-24.5 c-0.828,0-1.5-0.672-1.5-1.5v-4.125c0-0.828,0.672-1.5,1.5-1.5h24.5c0.828,0,1.5,0.672,1.5,1.5V37.816z M86.566,24.816 c0,0.828-0.672,1.5-1.5,1.5h-37.5c-0.828,0-1.5-0.672-1.5-1.5v-12.5c0-0.828,0.672-1.5,1.5-1.5h37.5c0.828,0,1.5,0.672,1.5,1.5 V24.816z"></path> </g> </g></svg>
+<SignedIn>
+        { isSignedIn && <div className=" flex text-header text-xl  md:text-2xl font-bold font-sans gap-3 text-white uppercase"> INVESTOR  {user.firstName}</div> }
+              </SignedIn>
+      <SignedOut>
+      <p className=' text-2xl font-bold font-sans gap-3 text-white'> FX INVESTOR</p>
+      </SignedOut>
+</div>
+<div className=" flex  flex-col align-middle items-center justify-center">
+<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M22 12C22 16.714 22 19.0711 20.5355 20.5355C19.0711 22 16.714 22 12 22C7.28595 22 4.92893 22 3.46447 20.5355C2 19.0711 2 16.714 2 12C2 7.28595 2 4.92893 3.46447 3.46447C4.92893 2 7.28595 2 12 2C16.714 2 19.0711 2 20.5355 3.46447C21.5093 4.43821 21.8356 5.80655 21.9449 8" stroke="#1C274D" stroke-width="1.5" stroke-linecap="round"></path> <path d="M18 8.49998H14M18 14.5H14M18 17.5H14M10 8.49999H8M8 8.49999L6 8.49999M8 8.49999L8 6.49998M8 8.49999L8 10.5M9.5 14.5L8.00001 16M8.00001 16L6.50001 17.5M8.00001 16L6.5 14.5M8.00001 16L9.49999 17.5" stroke="#1C274D" stroke-width="1.5" stroke-linecap="round"></path> </g></svg>
+<p className=' text-2xl font-bold font-sans gap-3 text-white'> CALCULATE</p>
+</div>
+      </div>
+    <div className='flex flex-row px-2 md:px-[20vw] justify-between pt-8 w-[100vw] bg-slate-700 mt-10 md:mt-20'>
       <motion.header
         onClick={() => setActiveTab('REFERRAL')}
         className={`text-black font-serif  md:text-2xl text-[20px]  font-bold md:mx-5 mx-2 pt-4  cursor-pointer ${activeTab === 'REFERRAL' ? 'bg-white     bg-clip-text text-transparent animate-gradient' : 'bg-slate-700'}`}
--- middleware.ts
+++ middleware.ts
@@ -5,10 +5,10 @@ import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
   // An array of public routes that don't require authentication.
-  publicRoutes: ["/","/dashboard/invest"],
+  publicRoutes: ["/"],
 
   // An array of routes to be ignored by the authentication middleware.
-  ignoredRoutes: ["/","/dashboard/invest"],
+  ignoredRoutes: ["/",],
 });
 
 export const config = {
