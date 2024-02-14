ebe2fbacfb32801b8d6937f6cbfc001552cc54e9
FirsT2015 <you@example.com>
2024-02-14 12:47:32 +0100

    iygh

--- components/invest/Basic2.tsx
+++ components/invest/Basic2.tsx
@@ -1,8 +1,48 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react';
+import { motion } from 'framer-motion';
+
 
 const Basic2 = () => {
+  const [activeTab, setActiveTab] = useState('Basic');
   return (
-    <div>Basic2</div>
+    <div>
+   <div className='flex flex-row md:mx-2 justify-between pt-14'>
+     <motion.header
+       onClick={() => setActiveTab('Basic')}
+       className={`text-slate-200 font-heading  text-2xl border-b-2 md:mx-5  mx-1 pt-4  cursor-pointer ${activeTab === 'Basic' ? 'border-green-700  rounded-md  bg-gradient-to-r from-red-500 to-sky-500 bg-clip-text text-transparent animate-gradient' : 'border-amber-100'}`}
+       animate={{ scale: activeTab === 'Basic' ? 1.2 : 1 }}
+     >
+    Basic
+     </motion.header>
+     <motion.header
+       onClick={() => setActiveTab('Advance')}
+       className={`text-slate-200 font-heading  text-2xl border-b-2 md:mx-5 mx-1 pt-4 backdrop:pt-4  cursor-pointer ${activeTab === 'Advance' ? 'border-green-700  rounded-md  bg-gradient-to-r from-red-500 to-sky-500 bg-clip-text text-transparent animate-gradient' : 'border-amber-100'}`}
+     >
+    Advance
+     </motion.header>
+     <motion.header
+       onClick={() => setActiveTab('Pro Plan')}
+       className={`text-slate-200 font-heading  text-2xl border-b-2 md:mx-5 mx-1 pt-4  cursor-pointer ${activeTab === 'Premium' ? 'border-green-700  rounded-md  bg-gradient-to-r from-red-500 to-sky-500 bg-clip-text text-transparent animate-gradient' : 'border-amber-100'}`}
+     >
+     Premium
+     </motion.header>
+     <motion.header
+       onClick={() => setActiveTab('Premium')}
+       className={`text-slate-200 font-heading  text-2xl border-b-2 md:mx-5 mx-1 pt-4  cursor-pointer ${activeTab === 'Pro Plan' ? 'border-green-700  rounded-md  bg-gradient-to-r from-red-500 to-sky-500 bg-clip-text text-transparent animate-gradient' : 'border-amber-100'}`}
+     >
+     Pro Plan
+     </motion.header>
+   </div>
+   <div  className=' h-1 w-full bg-slate-300 mt-2 mb-5'/>
+   {activeTab === 'Basic' && 
+   <div>
+    <h1></h1>
+  </div>}
+   {activeTab === 'Advance' && <div>yea</div>}
+   {activeTab === 'Pro Plan' && <div>nope</div>}
+   {activeTab === 'Premium' && <div>nopejuytgf</div>}
+ </div>
   )
 }
 
