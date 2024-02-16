5a74ad931fa8bb06ea48b88500d183d325acaee1
FirsT2015 <you@example.com>
2024-02-16 16:26:26 +0100

    kgfyugu

--- components/Basic.tsx
+++ components/Basic.tsx
@@ -2,15 +2,15 @@
 import React, { useState } from 'react';
 
 const Basic = () => {
-  const [basicPlan, setBasicPlan] = useState({ monthlyPrice: "5% Daily", items: ['Total Roll:  40%', 'Duration:  8 Days', 'Minium Deposit:  $  200', 'Maxium Deposit:  $  2000', '5% Referral Bonus']});
-  const [advancePlan, setAdvancePlan] = useState({ monthlyPrice: "5% Daily", items: ['Total Roll:  60%', 'Duration:  12 Days', 'Minium Deposit:  $  2,100', 'Maxium Deposit:  $  10,000', '5% Referral Bonus']});
-  const [proPlan, setProPlan] = useState({ monthlyPrice: "5% Daily", items: ['Total Roll:  80%', 'Duration:  14 Days', 'Minium Deposit:  $  10,100', 'Maxium Deposit:  $  20,000', '5% Referral Bonus' ]});
-  const [premiumPlan, setPremiumPlan] = useState({ monthlyPrice:  "10% Daily", items: ['Total Roll:  100%', 'Duration:  5 Days', 'Minium Deposit:  $  21,100', 'Maxium Deposit:  $  50,000', '5% Referral Bonus' ]});
+  const [basicPlan, setBasicPlan] = useState({ monthlyPrice: "5% Daily", items: ['Total Roll:   40%', 'Duration:   8 Days', 'Minium Deposit:  $   200', 'Maxium Deposit:  $   2000', '5% Referral Bonus']});
+  const [advancePlan, setAdvancePlan] = useState({ monthlyPrice: "5% Daily", items: ['Total Roll:   60%', 'Duration:   12 Days', 'Minium Deposit:  $   2,100', 'Maxium Deposit:  $   10,000', '5% Referral Bonus']});
+  const [proPlan, setProPlan] = useState({ monthlyPrice: "5% Daily", items: ['Total Roll:   80%', 'Duration:   14 Days', 'Minium Deposit:  $   10,100', 'Maxium Deposit:  $   20,000', '5% Referral Bonus' ]});
+  const [premiumPlan, setPremiumPlan] = useState({ monthlyPrice:  "10% Daily", items: ['Total Roll:   100%', 'Duration:   5 Days', 'Minium Deposit:  $   21,100', 'Maxium Deposit:  $   50,000', '5% Referral Bonus' ]});
 
   return (
     <div className="text-gray-900 sm:h-[200vh] font-sans h-[170vh] md:h-[120vh] lg:h-[100vh] mt-[20px]  bg-no-repeat bg-bottom sm:bg-bottom md:mx-[10vw] mx-[30px] mb-auto">
       <div className="flex flex-col md:flex-row md:transform md:scale-75 lg:scale-100 justify-center ">
-        {['Basic Plan, Advance Plan', 'Pro Plan', 'Premium Plan'].map((plan, index) => {
+        {['Basic Plan', 'Advance Plan', 'Pro Plan', 'Premium Plan'].map((plan, index) => {
           let currentPlan;
           if (plan === 'Basic Plan') {
             currentPlan = basicPlan;
