19ba922a61fd1664a23e7fe62d98f090ef9aac8b
FirsT2015 <you@example.com>
2024-02-16 17:00:34 +0100

    yuvctxcyv

--- components/dashboard/balanceshow.tsx
+++ components/dashboard/balanceshow.tsx
@@ -22,7 +22,7 @@ const BalanceShow = () => {
 
 
   return (
-    <div className="relative overflow-hidden bg-gradient-to-r from-blue-600 to-sky-200 rounded-2xl flex flex-col justify-start align-middle items-start gap-5 p-4 md:px-3 px-10">
+    <div className="relative overflow-hidden bg-gradient-to-r from-blue-600 to-sky-200 rounded-2xl flex flex-col justify-start align-middle items-start gap-5 p-4 h-40 md:px-3 px-10">
       <div className={`absolute w-full transition-transform duration-500 ease-in-out transform ${currentView === 'account' ? 'translate-x-0' : '-translate-x-full'}`}>
         <div className="div">
           <div className="flex flex-row justify-between gap-5 md:gap-10">
@@ -31,7 +31,7 @@ const BalanceShow = () => {
           </div>
         </div>
       </div>
-      <div className={`absolute w-full transition-transform duration-500 ease-in-out transform ${currentView === 'investment' ? 'translate-x-0' : 'translate-x-full'}`}>
+      <div className={`absolute w-full transition-transform duration-500 ease-in-out  transform ${currentView === 'investment' ? 'translate-x-0' : 'translate-x-full'}`}>
         <div className="div">
           <div className="flex flex-row justify-between gap-8">
             <p className="font-bold md:text-2xl sm:text-md text-xl font-sono gap-3">INVESTMENT BALANCE</p>
