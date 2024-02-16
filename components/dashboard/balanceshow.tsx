3dfc3c6ed03011ab7605a1fd79cf376c250fc818
FirsT2015 <you@example.com>
2024-02-16 23:30:02 +0100

    80tyvhbjnk

--- components/dashboard/balanceshow.tsx
+++ components/dashboard/balanceshow.tsx
@@ -27,39 +27,43 @@ const BalanceShow = () => {
 
   return (
     <div className="relative overflow-hidden bg-gradient-to-r from-blue-600 to-sky-200 rounded-2xl flex flex-col justify-start align-middle items-start gap-5 p-4 h-40 md:px-3 px-10">
-      <div className="w-full transition-transform duration-500 ease-in-out transform">
-        <div>
-          <div className="flex flex-row justify-between gap-5 md:gap-10">
-            <p className="font-bold md:text-2xl text-xl sm:text-md font-sono gap-3">ACCOUNT BALANCE</p>
-            <p className="font-bold md:text-xl text-md font-sono gap-3">$   0.000</p>
-          </div>
-        </div>
-      </div>
-      {currentView === 'investment' && (
+      {currentView === 'account' && (
         <div className="w-full transition-transform duration-500 ease-in-out transform">
-          <div>
-            <div className="flex flex-row justify-between gap-8">
-              <p className="font-bold md:text-2xl sm:text-md text-xl font-sono gap-3">INVESTMENT BALANCE</p>
-              <p className="font-bold text-md md:text-xl font-sono gap-3">
-                {investments[investmentIndex].balance} {investments[investmentIndex].symbol}
-              </p>
+          <div  className="flex flex-col justify-between gap-5 md:gap-10">
+            <div className="flex flex-col justify-start gap-5 md:gap-10">
+              <p className="font-bold md:text-2xl text-xl sm:text-md font-sono gap-3">ACCOUNT BALANCE</p>
+              <p className="font-bold md:text-3xl text-2xl font-serif gap-3">$   0.000</p>
             </div>
           </div>
-        </div>
-      )}
-      <div className="flex justify-center mt-4">
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-2"
-          onClick={() => setCurrentView('account')}
-        >
-          Show Account
-        </button>
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          <button
+          className=" text-white font-bold py-2 px-4 underline text=bold"
           onClick={() => setCurrentView('investment')}
         >
           Show Investment
         </button>
+        </div>
+      )}
+      {currentView === 'investment' && (
+        <div className="w-full transition-transform duration-500 ease-in-out transform">
+          <div>
+            <div className="flex flex-col justify-start gap-8">
+              <p className="font-bold md:text-2xl sm:text-md text-xl font-sono gap-3">INVESTMENT BALANCE</p>
+              <p className="font-bold md:text-3xl text-2xl font-serif gap-3">
+                {investments[investmentIndex].balance} {investments[investmentIndex].symbol}
+              </p>
+            </div>
+          </div>
+          <button
+          className="text-white font-bold py-2 px-4 underline text=bold"
+          onClick={() => setCurrentView('account')}
+        >
+          Show Account
+        </button>
+        </div>
+      )}
+      <div className="flex justify-center mt-4">
+
+
       </div>
     </div>
   );
