b5eb19132c9abcf64a53a1a0a3865016814c3bdc
FirsT2015 <you@example.com>
2024-02-16 23:58:47 +0100

    tocvuybjkn yu

--- components/FAQ.tsx
+++ components/FAQ.tsx
@@ -4,13 +4,13 @@ import {Accordion, AccordionItem} from "@nextui-org/react";
 
 export default function FAQ() {
   const defaultContent =
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+    "Yes, Binance FX.com is registered in the Seychelles as “Binance FX”.";
 
   return (
     <div className=" flex flex-col justify-center align-middle items-center md:p-[40px] p-[10px] py-[10]">
       <div className=" text-white justify-center align-middle text-3xl  md:text-[4rem] font-extrabold font-serif p-2">Frequent questions ?</div>
     <Accordion variant="splitted" className=" text-black p-8">
-      <AccordionItem key="1" aria-label="Accordion 1" title="Accordion 1" className=" text-black text-xl">
+      <AccordionItem key="1" aria-label="Is Binance Fx a registered company" title="Is Binance Fx a registered company" className=" text-black text-xl">
         {defaultContent}
       </AccordionItem>
       <AccordionItem key="2" aria-label="Accordion 2" title="Accordion 2">
