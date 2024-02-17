6b73a725648b52c50d2035b8749acd37e48fc0af
FirsT2015 <you@example.com>
2024-02-17 07:46:32 +0100

    kvblhjm

--- components/dashboard/balanceshow.tsx
+++ components/dashboard/balanceshow.tsx
@@ -29,11 +29,12 @@ const BalanceShow = () => {
     <div className="relative overflow-hidden bg-gradient-to-r from-blue-600 to-sky-200 rounded-2xl flex flex-col justify-start align-middle items-start gap-5 p-4 h-40 md:px-3 px-10">
       {currentView === 'account' && (
         <div className="w-full transition-transform duration-500 ease-in-out transform">
-          <div  className="flex flex-col justify-between gap-5 md:gap-10">
+          <div  className="flex flex-row justify-between ">
             <div className="flex flex-col justify-start gap-5 md:gap-10">
               <p className="font-bold md:text-2xl text-xl sm:text-md font-sono gap-3">ACCOUNT BALANCE</p>
               <p className="font-bold md:text-3xl text-2xl font-serif gap-3">$   0.000</p>
             </div>
+            <div>eye</div>
           </div>
           <button
           className=" text-white font-bold py-2 px-4 underline text=bold"
@@ -45,13 +46,14 @@ const BalanceShow = () => {
       )}
       {currentView === 'investment' && (
         <div className="w-full transition-transform duration-500 ease-in-out transform">
-          <div>
+          <div className="flex flex-row justify-between ">
             <div className="flex flex-col justify-start gap-8">
               <p className="font-bold md:text-2xl sm:text-md text-xl font-sono gap-3">INVESTMENT BALANCE</p>
               <p className="font-bold md:text-3xl text-2xl font-serif gap-3">
                 {investments[investmentIndex].balance} {investments[investmentIndex].symbol}
               </p>
             </div>
+            <div>eye</div>
           </div>
           <button
           className="text-white font-bold py-2 px-4 underline text=bold"
