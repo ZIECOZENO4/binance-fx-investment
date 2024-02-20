808f06d568c648f9c76278c4adc811bd92075b39
FirsT2015 <you@example.com>
2024-02-20 14:01:46 +0100

    nhig89vuyi;bnkl

--- components/rate.tsx
+++ components/rate.tsx
@@ -1,9 +1,85 @@
-import React from 'react'
+"use client"
+import React, { useEffect } from 'react';
 
 const Rate = () => {
-  return (
-    <div>Rate</div>
-  )
-}
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://s3.tradingview.com/external-embedding/embed-widget-ticker-tape.js';
+    script.async = true;
+    script.innerHTML = JSON.stringify({
+      "symbols": [
+        {
+            "proName": "FOREXCOM:SPXUSD",
+            "title": "S&P 500"
+          },
+          {
+            "proName": "FOREXCOM:NSXUSD",
+            "title": "US 100"
+          },
+          {
+            "proName": "FX_IDC:EURUSD",
+            "title": "EUR to USD"
+          },
+          {
+            "proName": "BITSTAMP:BTCUSD",
+            "title": "Bitcoin"
+          },
+          {
+            "proName": "BITSTAMP:ETHUSD",
+            "title": "Ethereum"
+          },
+          {
+            "description": "",
+            "proName": "BINANCE:BTCUSDT"
+          },
+          {
+            "description": "",
+            "proName": "FX:USDJPY"
+          },
+          {
+            "description": "",
+            "proName": "TVC:GOLD"
+          },
+          {
+            "description": "",
+            "proName": "BITSTAMP:ETHUSD"
+          },
+          {
+            "description": "",
+            "proName": "FX:USDCAD"
+          },
+          {
+            "description": "",
+            "proName": "CRYPTOCAP:BTC.D"
+          }
+      ],
+      "showSymbolLogo": true,
+      "isTransparent": false,
+      "displayMode": "adaptive",
+      "colorTheme": "dark",
+      "locale": "en"
+    });
 
-export default Rate
+    const widgetContainer = document.querySelector('.tradingview-widget-container__widget');
+    if (widgetContainer) {
+      widgetContainer.appendChild(script);
+    }
+
+    return () => {
+      if (widgetContainer) {
+        widgetContainer.removeChild(script);
+      }
+    };
+  }, []);
+
+  return (
+    <div className="tradingview-widget-container mt-4">
+      <div className="tradingview-widget-container__widget"></div>
+      <div className="tradingview-widget-copyright">
+    
+      </div>
+    </div>
+  );
+};
+
+export default Rate;
