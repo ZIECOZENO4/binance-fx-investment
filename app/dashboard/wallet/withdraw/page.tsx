bdb8c4cd74e3faac7bb989b2b7393e298e2cc434
FirsT2015 <you@example.com>
2024-02-25 12:53:35 +0100

    dhfgnv

--- app/dashboard/wallet/withdraw/page.tsx
+++ app/dashboard/wallet/withdraw/page.tsx
@@ -1,31 +1,28 @@
 import { useAuth } from '@clerk/clerk-react';
 import { createClient } from "@supabase/supabase-js";
- 
+  
 const supabaseClient = async (supabaseAccessToken: string) => {
     const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_KEY, {
       global: { headers: { Authorization: `Bearer ${supabaseAccessToken}` } },
     });
-    // set Supabase JWT on the client object,
-    // so it is sent up with all Supabase requests
     return supabase;
-  };
-  
+};
+   
 function App() {
   const { getToken } = useAuth();
- 
+  
   const fetchData = async () => {
-    // TODO #1: Replace with your JWT template name
     const supabaseAccessToken = await getToken({ template: 'BinanceFX' });
- 
-    const supabase = await supabaseClient(supabaseAccessToken: string);
-    
-    // TODO #2: Replace with your database table name
+  
+    // Corrected function call: Pass the variable directly without specifying its type
+    const supabase = await supabaseClient(supabaseAccessToken as string);
+
     
     const { data, error } = await supabase.from('user_id').select();
- 
-    // TODO #3: Handle the response
+  
+    // Handle the response as needed
   };
- 
+  
   return (
     <div className="app">
       <button onClick={fetchData}>Fetch data</button>
