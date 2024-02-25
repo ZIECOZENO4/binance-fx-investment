804b0bfb806d1ffdb89baa73f525c4fa743edb0d
FirsT2015 <you@example.com>
2024-02-25 14:15:50 +0100

    kbiuvyg

--- app/dashboard/wallet/withdraw/page.jsx
+++ app/dashboard/wallet/withdraw/page.jsx
@@ -1,7 +1,7 @@
 import { useAuth } from '@clerk/clerk-react';
 import { createClient } from "@supabase/supabase-js";
   
-const supabaseClient = async (supabaseAccessToken: string) => {
+const supabaseClient = async (supabaseAccessToken) => {
     const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_KEY, {
       global: { headers: { Authorization: `Bearer ${supabaseAccessToken}` } },
     });
@@ -15,7 +15,7 @@ function App() {
     const supabaseAccessToken = await getToken({ template: 'BinanceFX' });
   
     // Corrected function call: Pass the variable directly without specifying its type
-    const supabase = await supabaseClient(supabaseAccessToken as string);
+    const supabase = await supabaseClient(supabaseAccessToken);
 
     
     const { data, error } = await supabase.from('user_id').select();
