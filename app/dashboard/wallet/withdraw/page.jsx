a8073f02cc8d4d258cac220d7a6a2fd0645ce764
FirsT2015 <you@example.com>
2024-02-25 15:09:58 +0100

    n'ibo;kj

--- app/dashboard/wallet/withdraw/page.jsx
+++ app/dashboard/wallet/withdraw/page.jsx
@@ -1,31 +1,42 @@
-import { useAuth } from '@clerk/clerk-react';
-import { createClient } from "@supabase/supabase-js";
+// import { useAuth } from '@clerk/clerk-react';
+// import { createClient } from "@supabase/supabase-js";
   
-const supabaseClient = async (supabaseAccessToken) => {
-    const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_KEY, {
-      global: { headers: { Authorization: `Bearer ${supabaseAccessToken}` } },
-    });
-    return supabase;
-};
+// const supabaseClient = async (supabaseAccessToken) => {
+//     const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_KEY, {
+//       global: { headers: { Authorization: `Bearer ${supabaseAccessToken}` } },
+//     });
+//     return supabase;
+// };
    
-function App() {
-  const { getToken } = useAuth();
+// function App() {
+//   const { getToken } = useAuth();
   
-  const fetchData = async () => {
-    const supabaseAccessToken = await getToken({ template: 'BinanceFX' });
+//   const fetchData = async () => {
+//     const supabaseAccessToken = await getToken({ template: 'BinanceFX' });
   
-    // Corrected function call: Pass the variable directly without specifying its type
-    const supabase = await supabaseClient(supabaseAccessToken);
+//     // Corrected function call: Pass the variable directly without specifying its type
+//     const supabase = await supabaseClient(supabaseAccessToken);
 
     
-    const { data, error } = await supabase.from('user_id').select();
+//     const { data, error } = await supabase.from('user_id').select();
   
-    // Handle the response as needed
-  };
+//     // Handle the response as needed
+//   };
   
+//   return (
+//     <div className="app">
+//       <button onClick={fetchData}>Fetch user_id</button>
+//     </div>
+//   );
+// }
+
+
+import React from 'react'
+
+const Page = () => {
   return (
-    <div className="app">
-      <button onClick={fetchData}>Fetch user_id</button>
-    </div>
-  );
+    <div>Page</div>
+  )
 }
+
+export default Page
