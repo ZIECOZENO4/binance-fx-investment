69fab6e2c00264c553694f48f851120b773fcf03
FirsT2015 <you@example.com>
2024-02-25 19:57:52 +0100

    i8g7uionp'

--- components/withdraw/withdraw.js
+++ components/withdraw/withdraw.js
@@ -57,10 +57,10 @@ const WithdrawalPage = () => {
             <div className="mt-14 gap-3 flex flex-col  text-white font-bold justify-start items-start align-middle">
           <p className="block font-bold text-white text-2xl md:text-2xl justify-center items-center align-middle mr-4"> Withdrawal Process</p>
           <div className="mt-8 gap-6 flex flex-row  justify-start items-start align-middle w-[100vw]">
-  <h1 className="font-bold text-white text-xl md:text-2xl w-[50vw] ">Withdraw From :</h1>
+  <h1 className="font-bold text-white text-xl md:text-2xl w-[40vw] ">Withdraw From :</h1>
 
 
-  <div class="flex  items-center border-l-8 border-emerald-500 bg-emerald-50 p-4 text-emerald-900 shadow-lg  w-[50vw] ">
+  <div class="flex  items-center border-l-8 border-emerald-500 bg-emerald-50 p-4 text-emerald-900 shadow-lg  w-[60vw] ">
     <div class="min-w-0">
       <h2 class="overflow-hidden text-ellipsis whitespace-nowrap">Binance FX</h2>
     </div>
@@ -68,10 +68,10 @@ const WithdrawalPage = () => {
 
 </div>
 <div className="mt-8 gap-2 flex flex-row  justify-start items-start align-middle  w-[100vw] ">
-  <h1 className="font-bold text-white text-xl md:text-2xl   w-[50vw] ">Withdrawer&apos;s Name :</h1>
+  <h1 className="font-bold text-white text-xl md:text-2xl   w-[40vw] ">Withdrawer&apos;s Name :</h1>
 
 
-  <div class="flex max-w-md items-center border-l-8 border-emerald-500 bg-emerald-50 p-2  w-[50vw] text-emerald-900 shadow-lg">
+  <div class="flex max-w-md items-center border-l-8 border-emerald-500 bg-emerald-50 p-2  w-[60vw] text-emerald-900 shadow-lg">
     <div class="min-w-0">
       
       <SignedIn>
@@ -83,10 +83,10 @@ const WithdrawalPage = () => {
 
 </div>
 <div className="mt-8 gap-6 flex flex-row  justify-start items-start align-middle  w-[100vw] ">
-  <h1 className="font-bold text-white text-xl md:text-2xl  w-[50vw]   ">Available Balance :</h1>
+  <h1 className="font-bold text-white text-xl md:text-2xl  w-[40vw]   ">Available Balance :</h1>
 
 
-  <div class="flex max-w-md items-center border-l-8 border-emerald-500 bg-emerald-50 p-2  w-[50vw] text-emerald-900 shadow-lg">
+  <div class="flex max-w-md items-center border-l-8 border-emerald-500 bg-emerald-50 p-2  w-[60vw] text-emerald-900 shadow-lg">
     <div class="min-w-0">
  <h2 class="overflow-hidden text-ellipsis  whitespace-nowrap">0.00</h2>
     </div>
@@ -94,10 +94,10 @@ const WithdrawalPage = () => {
 
 </div>
 <div className="mt-8 gap-6 flex flex-row  justify-start items-start align-middle  w-[100vw] ">
-  <h1 className="font-bold text-white text-xl md:text-2xl  w-[50vw]  ">Transaction Time :</h1>
+  <h1 className="font-bold text-white text-xl md:text-2xl  w-[40vw]  ">Transaction Time :</h1>
 
 
-  <div class="flex max-w-md items-center border-l-8 border-emerald-500 bg-emerald-50 p-2 text-emerald-900 shadow-lg  w-[50vw] ">
+  <div class="flex max-w-md items-center border-l-8 border-emerald-500 bg-emerald-50 p-2 text-emerald-900 shadow-lg  w-[60vw] ">
     <div class="min-w-0">
   <h2 class="overflow-hidden text-ellipsis font-bold  whitespace-nowrap">{new Date().toLocaleTimeString()}</h2>
 
@@ -106,10 +106,10 @@ const WithdrawalPage = () => {
 
 </div>
 <div className="mt-8 gap-6 flex flex-row  justify-start items-start align-middle  w-[100vw] ">
-  <h1 className="font-bold text-white text-xl md:text-2xl  w-[50vw]  ">Gass Fee :</h1>
+  <h1 className="font-bold text-white text-xl md:text-2xl  w-[40vw]  ">Gass Fee :</h1>
 
 
-  <div class="flex max-w-md items-center border-l-8 border-emerald-500 bg-emerald-50 p-2 text-emerald-900 shadow-lg  w-[50vw] ">
+  <div class="flex max-w-md items-center border-l-8 border-emerald-500 bg-emerald-50 p-2 text-emerald-900 shadow-lg  w-[60vw] ">
     <div class="min-w-0">
   <h2 class="overflow-hidden text-ellipsis font-bold  whitespace-nowrap">0.00234123 wei</h2>
 
@@ -119,7 +119,7 @@ const WithdrawalPage = () => {
 </div>
 <div>
 <div className="p-4">
-      <div className="flex justify-between items-center mb-4">
+      <div className="flex justify-center flex-col items-center mb-4">
    
            <div className="mt-8 gap-6 flex flex-row   justify-between items-start align-middle">
   <h1 className="font-bold text-white text-xl md:text-2xl  w-[30vw]  ">Amount :</h1>
@@ -162,22 +162,23 @@ const WithdrawalPage = () => {
    
       </div>
   
-      <div className=' flex flex-row justify-between align-middle items-center gap-4 px-3 py-5  w-[100vw] '>
+      <div className=' flex flex-row justify-between align-middle items-center gap-4 px-2 py-5  w-[100vw] '>
+       <button
+        className="bg-green-500 text-white px-4 py-2 rounded  w-[40vw] "
+       
+      >
+        Share
+      </button>
     <Button
       disableRipple
-      className="relative flex flex-col m-2  h-auto align-middle overflow-visible rounded-full hover:-translate-y-1 px-6 shadow-xl bg-background/30 after:content-[''] after:absolute after:rounded-full after:inset-0 after:bg-background/40 after:z-[-1] after:transition after:!duration-500 hover:after:scale-150 hover:after:opacity-0  w-[60vw] "
+      className="relative flex flex-col m-2  h-12 align-middle overflow-visible rounded-full hover:-translate-y-1 px-6 shadow-xl bg-background/30 after:content-[''] after:absolute after:rounded-full after:inset-0 after:bg-background/40 after:z-[-1] after:transition after:!duration-500 hover:after:scale-150 hover:after:opacity-0  w-[60vw] "
       size="lg"
       onClick={handleWithdraw}
     >
 
       <p className="font-bold text-white">Withdraw</p>  
     </Button>
-    <button
-        className="bg-green-500 text-white px-4 py-2 rounded  w-[40vw] "
-       
-      >
-        Share
-      </button>
+   
       </div>
   
 
