934ffae9bb11c037ab3709b3332bcff52b88d9c4
FirsT2015 <you@example.com>
2024-02-26 19:28:19 +0100

    o0-yt7fgphio'p

--- utils/storeUserInSupabase.js
+++ utils/storeUserInSupabase.js
@@ -3,8 +3,9 @@ import supabase from './supabaseClient';
 
 async function storeUserInSupabase(user) {
   const { data, error } = await supabase
-    .from('users')
-    .insert([{ id: user.id, email: user.email,  name: user.firstName,  username: user.username }], { upsert: true });
+  .from('users')
+  .insert([{ user_id: user.id, email: user.email, name: user.firstName, username: user.username }], { upsert: true })
+  .headers({ 'apikey': 'your_supabase_secret_key' });
   if (error) {
     console.error('Error storing user in Supabase:', error);
     return false;
