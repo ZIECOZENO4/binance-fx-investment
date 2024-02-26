35a1f8eeb19621b20fd311a7d6db846dadfdf013
FirsT2015 <you@example.com>
2024-02-26 21:23:24 +0100

    log'098pfocy;bn/kl

--- app/dashboard/fx-mining/page.tsx
+++ app/dashboard/fx-mining/page.tsx
@@ -2,7 +2,36 @@ import React from 'react'
 
 const Page = () => {
   return (
-    <div>Page</div>
+    <div>
+
+<div className="relative isolate overflow-hidden bg-green-500">
+
+
+  <div className="mx-auto w-full px-6 pb-24 pt-10 sm:pb-32 lg:flex lg:px-8 lg:py-40">
+    <div className="mx-auto max-w-2xl flex-shrink-0 lg:mx-0 lg:max-w-xl lg:pt-8">
+
+      <div className="mt-24 sm:mt-32 lg:mt-16">
+        <a href="#" className="inline-flex space-x-6">
+          <span className="rounded-full bg-indigo-500/10 px-3 py-1 text-sm font-semibold leading-6 text-gray-8`w  00 ring-1 ring-inset ring-indigo-500/20">What&apos;s new</span>
+          <span className="inline-flex items-center space-x-2 text-sm font-medium leading-6 text-gray-300">
+            <span>Coming Soon</span>
+            <svg className="h-5 w-5 text-gray-500" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+              <path fill-rule="evenodd" d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clip-rule="evenodd" />
+            </svg>
+          </span>
+        </a>
+      </div>
+      <h1 className="mt-10 text-4xl font-bold tracking-tight text-white sm:text-6xl">Binance FX Coin and Token</h1>
+      <p className="mt-6 text-lg leading-8 text-gray-300">The mining has not yet </p>
+      <div className="mt-10 flex items-center gap-x-6">
+        <a href="#" className="rounded-md bg-teal-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-teal-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600">Awaiting</a>
+        <a href="#" className="text-sm font-semibold leading-6 text-white">Join Waitlist <span aria-hidden="true">→</span></a>
+      </div>
+    </div>
+
+  </div>
+</div>
+    </div>
   )
 }
 
--- app/dashboard/sell/page.tsx
+++ app/dashboard/sell/page.tsx
@@ -2,7 +2,26 @@ import React from 'react'
 
 const Page = () => {
   return (
-    <div>Page</div>
+    <div>
+
+<div className="relative isolate overflow-x-hidden bg-black">
+
+
+  <div className="mx-auto max-w-7xl px-6 pb-24 pt-10 sm:pb-32 lg:flex lg:px-8 lg:py-40">
+    <div className="mx-auto max-w-2xl flex-shrink-0 lg:mx-0 lg:max-w-xl lg:pt-8">
+
+    
+      <h1 className="mt-10 text-4xl font-bold tracking-tight text-white sm:text-6xl">FX coin</h1>
+      <p className="mt-6 text-lg leading-8 text-gray-300">Not yet available, go to mining page to mine your FX coin</p>
+      <div className="mt-10 flex items-center gap-x-6">
+        <a href="/dashboard/fx-mining" className="rounded-md bg-teal-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-teal-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600">Mine now</a>
+        <a href="#" className="text-sm font-semibold leading-6 text-white">Keep waiting <span aria-hidden="true">→</span></a>
+      </div>
+    </div>
+
+  </div>
+</div>
+    </div>
   )
 }
 
