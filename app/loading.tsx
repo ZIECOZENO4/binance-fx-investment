43ebedd8fc9cd53b10e68b875254741fb19f60e2
FirsT2015 <you@example.com>
2024-02-26 21:37:47 +0100

    op908filuo

--- app/loading.tsx
+++ app/loading.tsx
@@ -5,6 +5,7 @@ import AppLoader from '@/components/loading'
 
 function Loading() {
   return (
+    
     <div className=' bg-black justify-center m-0 p-0 flex align-middle items-center  w-full h-[100vh]' >  
     <AppLoader />
     <div className="mt-5 text-center text-blue-400">
