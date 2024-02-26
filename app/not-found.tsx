fdf031ca5a29267353f99a7f727bb1d010b22c8b
FirsT2015 <you@example.com>
2024-02-26 20:01:38 +0100

    hp'bnopm

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -8,8 +8,8 @@ export default function NotFound () {
                 <div className="w-full md:w-1/2 flex flex-col items-center justify-center lg:px-2 xl:px-0 text-center">
                     <p className="text-4xl md:text-[4rem] lg:text-[5rem] font-bold tracking-wider text-gray-300">404</p>
                     <p className="text-3xl md:text-4xl lg:text-5xl font-bold tracking-wider text-gray-300 mt-2">Page Not Found</p>
-                    <p className="text-lg md:text-xl lg:text-2xl text-gray-500 my-12 sm:mx-2">Sorry,Beta Sporting couldn&apos;t find or load the page you are expecting, kindly contact us if it persists.</p>
-                    <Link href="/" className="flex items-center space-x-2 mb-2 md:mb-4  bg-green-600 hover:bg-green-700 text-gray-100 px-4 py-2 rounded transition duration-150" title="Return Home">
+                    <p className="text-lg md:text-xl lg:text-2xl text-gray-500 my-12 sm:mx-2">Sorry,Binance FX couldn&apos;t find or load the page you are expecting, kindly contact us if it persists.</p>
+                    <Link href="/dashboard" className="flex items-center space-x-2 mb-2 md:mb-4  bg-green-600 hover:bg-green-700 text-gray-100 px-4 py-2 rounded transition duration-150" title="Return Home">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0  0  20  20" fill="currentColor">
                             <path fill-rule="evenodd" d="M9.707  14.707a1  1  0  01-1.414  0l-4-4a1  1  0  010-1.414l4-4a1  1  0  011.414  1.414L7.414  9H15a1  1  0  110  2H7.414l2.293  2.293a1  1  0  010  1.414z" clip-rule="evenodd"></path>
                         </svg>
--- utils/storeUserInSupabase.js
+++ utils/storeUserInSupabase.js
@@ -3,9 +3,9 @@ import supabase from './supabaseClient';
 
 async function storeUserInSupabase(user) {
   const { data, error } = await supabase
-  .from('users')
-  .insert([{ user_id: user.id, email: user.email, name: user.firstName, username: user.username }], { upsert: true })
-  .headers({ 'apikey': 'your_supabase_secret_key' });
+    .from('users')
+    .insert([{ user_id: user.id, email: user.email, name: user.firstName, username: user.username }], { upsert: true });
+
   if (error) {
     console.error('Error storing user in Supabase:', error);
     return false;
