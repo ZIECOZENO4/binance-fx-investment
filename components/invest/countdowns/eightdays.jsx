645b46d44e28c866d8342acc922e25367e19443a
FirsT2015 <you@example.com>
2024-03-01 12:24:12 +0100

    iufydv.k/bln;

--- components/invest/countdowns/eightdays.jsx
+++ components/invest/countdowns/eightdays.jsx
@@ -68,21 +68,21 @@ export default function EightDays({ id }) {
  );
 }
 
-function ChevronRightIcon(props: ChevronRightIconProps) {
- return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="m9 18 6-6-6-6" />
-    </svg>
- );
-}
+function ChevronRightIcon(props) {
+  return (
+     <svg
+       {...props}
+       xmlns="http://www.w3.org/2000/svg"
+       width="24"
+       height="24"
+       viewBox="0 0 24 24"
+       fill="none"
+       stroke="currentColor"
+       strokeWidth="2"
+       strokeLinecap="round"
+       strokeLinejoin="round"
+     >
+       <path d="m9 18 6-6-6-6" />
+     </svg>
+  );
+ }
