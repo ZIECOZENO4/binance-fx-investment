d6f125e84180bc62556fa37a6b14ff2d94f374de
FirsT2015 <you@example.com>
2024-03-06 17:14:50 +0100

    klobigufytxghkjlk;ln;

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/components/navbar";
-<<<<<<< HEAD
 import { Providers as TenstackProviders } from "./tenstack-provider";
-import { Providers } from "./providers";
-=======
->>>>>>> c45e6e773f58bb64d9bcf080b739e8dee477bd9e
 import SmNavbar from "@/components/Sm NavBar";
 import dynamic from 'next/dynamic';
 import { Suspense } from 'react';
@@ -28,34 +24,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} overflow-x-hidden`}>
-<<<<<<< HEAD
-      <TenstackProviders>
-=======
+      <body className={`${inter.className} overflow-x-hidden`}>  
         <TenstackProviders>
->>>>>>> c45e6e773f58bb64d9bcf080b739e8dee477bd9e
-          <Providers>
+        <Providers>
             <div className=" w-[100vw] bg-black ">
               <div className='w-[100vw] z-30 sticky'>
                 <NavBar />
                 <SmNavbar />
               </div>
-<<<<<<< HEAD
-=======
-
->>>>>>> c45e6e773f58bb64d9bcf080b739e8dee477bd9e
               <Suspense fallback={<Loading />}>
                 {children}
               </Suspense>
             </div>
           </Providers>
         </TenstackProviders>
-<<<<<<< HEAD
- 
         </body>
-=======
-      </body>
->>>>>>> c45e6e773f58bb64d9bcf080b739e8dee477bd9e
+
     </html>
   );
 }
