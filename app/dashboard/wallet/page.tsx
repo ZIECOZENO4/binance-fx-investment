ca5f9051743da85112bd231d8a667887b5b36115
FirsT2015 <you@example.com>
2024-03-07 13:18:09 +0100

    kio;fuchjvk/bl

--- app/dashboard/wallet/page.tsx
+++ app/dashboard/wallet/page.tsx
@@ -1,17 +1,11 @@
 "use client"
-import { useUser } from '@clerk/clerk-react';
 import Wallet from '@/components/wallet/wallet'
 import React from 'react'
 
 const WalletPage= () => {
-  const { user } = useUser();
-
-  // Provide a fallback value for userId
-  const userId = user ? user.id : 'defaultUserId'; // Replace 'defaultUserId' with an appropriate fallback
- 
  return (
     <div>
-      <Wallet userId={userId} />
+      <Wallet  />
     </div>
  );
 };
