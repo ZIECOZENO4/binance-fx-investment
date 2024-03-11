a0d9e159d6dfd0a37c49038fe6c2c0d6de8f3e5e
ZIECOZENO4 <ziecozeno4@gmail.com>
2024-03-11 13:12:13 +0100

    yilcf97;uicvugy

---
+++ app/dashboard/invest/confirmpayment/ConfirmationPopup.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+interface ConfirmationPopupProps {
+ plan: string;
+ planId: string;
+ amount: number;
+ onClose: () => void; // Specify the type for the onClose function
+}
+
+// Use the interface in your ConfirmationPopup component
+const ConfirmationPopup: React.FC<ConfirmationPopupProps> = ({ plan, planId, amount, onClose }) => {
+ return (
+    <div className="fixed inset-0 flex items-center justify-center z-50">
+      <div className="bg-white p-4 rounded shadow-lg">
+        <h2>Plan: {plan}</h2>
+        <p>Plan ID: {planId}</p>
+        <p>Amount: {amount}</p>
+        <p>Wait for confirmation...</p>
+        <button onClick={onClose} className="bg-blue-500 text-white px-4 py-2 rounded">OK</button>
+      </div>
+    </div>
+ );
+};
+
+export default ConfirmationPopup;
--- components/dashboard/shared/Bottombar.tsx
+++ components/dashboard/shared/Bottombar.tsx
@@ -9,23 +9,27 @@ function Bottombar() {
   const router = useRouter();
   const pathname = usePathname();
   return (
-     <section className='fixed bottom-0 z-10 w-full rounded-t-3xl bg-glassmorphism p-4 backdrop-blur-lg xs:px-7 md:hidden bg-gradient-to-t from-slate-600 to-slate-950'>
-        <div className='flex items-center justify-between gap-3 xs:gap-5'>
+    <div className='fixed bottom-0 z-10 w-full bg-glassmorphism p-2 backdrop-blur-lg xs:px-4 md:hidden'>
+   <hr className=" bg-blue-600"/>
+     <section className=' bg-gradient-to-t from-slate-600 to-slate-950'>
+        <div className='flex items-center justify-between gap-3 xs:gap-3'>
          {
            bottomLinks.map((link) => {
-             // Check if the current pathname exactly matches the link's route
+        
              const isActive = pathname === link.route;
  
              return (
-               <Link href={link.route} key={link.label} className={`relative flex flex-col items-center gap-2 rounded-lg p-2 sm:flex-1 sm:px-2 sm:py-1 ${isActive ? ' text-blue-700' : ''}`}>
-                 <Image src={link.imgURL} alt={link.label} width={20} height={20}/>
-                 <p className={`text-blue-700 text-light-1 max-sm:text-[20px] ${isActive ? ' text-black' : ''}`}>{link.label.split(/\s+/)[0]}</p>
-               </Link>
+              <Link href={link.route} key={link.label} className={`relative flex flex-col items-center gap-2 rounded-lg ${isActive ? 'bg-blue-700 text-white' : 'bg-white text-black'}`}>
+                 <Image src={link.imgURL} alt={link.label} width={30} height={30} className={`${isActive ? 'filter-invert' : ''}`} />
+                 <p className={`${isActive ? 'text-white' : 'text-black'}`}>{link.label.split(/\s+/)[0]}</p>
+              </Link>
              );
            })
          }
        </div>   
      </section>
+    </div>
+ 
   )
  }
  
