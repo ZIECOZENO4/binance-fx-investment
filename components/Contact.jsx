5c42b2f589eaf1cd43e8bfeec0d1fa30fab4fe95
ZIECOZENO4 <ziecozeno4@gmail.com>
2024-03-18 17:40:28 +0100

    jvuf

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -108,7 +108,7 @@ const Contact = () => {
               name='message'
               value={form.message}
               onChange={handleChange}
-              placeholder='What you want to say?'
+              placeholder='What you want to say or contribute?'
               className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
             />
           </label>
