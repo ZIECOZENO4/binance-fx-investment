d8905ff28d41202c85ed0ee807b1f6c1168fb62b
ZIECOZENO4 <ziecozeno4@gmail.com>
2024-03-20 00:03:44 +0100

    buijhln.m

---
+++ app/dashboard/trade/layout.tsx
@@ -0,0 +1,42 @@
+'use client'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import CryptoContext from "../../../CryptoContext";
+
+import { Providers as TenstackProviders } from "../../tenstack-provider";
+import Topbar from "@/components/dashboard/shared/Topbar";
+import LeftSidebar from "@/components/dashboard/shared/Leftsidebar";
+import Promos from "@/components/dashboard/shared/Rightsidebar";
+import Bottombar from "@/components/dashboard/shared/Bottombar";
+
+const inter = Inter({ subsets: ["latin"] });
+
+
+const activeChain = "ethereum";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <TenstackProviders>
+ <CryptoContext>
+      <div className={inter.className}>
+      <Topbar />
+
+<main className='flex flex-row'>
+  <LeftSidebar />
+  <section className='main-container'>
+    <div className='w-[100vw] max-w-4xl'>{children}</div>
+  
+  </section>
+   <Promos />
+</main>
+<div className="h-[100px] w-full bg-black md:hidden"/>
+<Bottombar />
+        </div>
+        </CryptoContext>
+            </TenstackProviders>
+  );
+}
