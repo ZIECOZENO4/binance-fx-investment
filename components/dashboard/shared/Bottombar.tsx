36d717ee91e53e8ff2bc2bf5a43b94c11de0ca65
ZIECOZENO4 <ziecozeno4@gmail.com>
2024-03-20 19:25:32 +0100

    jkvuiycvijk

--- components/dashboard/shared/Bottombar.tsx
+++ components/dashboard/shared/Bottombar.tsx
@@ -1,36 +1,33 @@
 'use client'
 import Image from 'next/image'
-import {bottomLinks} from '../../../app/constants/index'
+import { bottomLinks } from '../../../app/constants/index'
 import React from 'react'
 import Link from 'next/link'
-import { usePathname, useRouter} from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 
 function Bottombar() {
   const router = useRouter();
   const pathname = usePathname();
   return (
-    <div className='fixed bottom-0 z-10 w-full bg-glassmorphism p-2 backdrop-blur-lg xs:px-4 md:hidden'>
-   <hr className=" bg-sky-700"/>
-     <section className=' bg-gradient-to-t from-slate-600 to-slate-950'>
+    <div className='fixed bottom-0 z-10 w-full bg-glassmorphism  backdrop-blur-lg mt-2 md:hidden'>
+      <hr className="bg-sky-700 py-1"/>
+      <section className='bg-gradient-to-t from-slate-600 to-slate-950'>
         <div className='flex items-center justify-between gap-3 xs:gap-3'>
-         {
-           bottomLinks.map((link) => {
-        
-             const isActive = pathname === link.route;
- 
-             return (
-              <Link href={link.route} key={link.label} className={`relative flex flex-col items-center  rounded-lg ${isActive ? 'bg-blue-700 text-white text-sm' : 'text-blue-600 text-sm'}`}>
-                 <Image src={link.imgURL} alt={link.label} width={35} height={35} className={`${isActive ? 'filter-invert' : ''}`} />
-                 <p className={`${isActive ? 'text-white text-sm' : 'text-blue-600 text-sm'}`}>{link.label.split(/\s+/)[0]}</p>
-              </Link>
-             );
-           })
-         }
-       </div>   
-     </section>
+          {
+            bottomLinks.map((link) => {
+              const isActive = pathname === link.route;
+              return (
+                <Link href={link.route} key={link.label} className={`relative flex flex-col items-center rounded-lg ${isActive ? 'bg-blue-700 text-white border-spacing-[2px]' : 'text-blue-600'} text-xs`}>
+                  <Image src={link.imgURL} alt={link.label} width={30} height={30} className={`${isActive ? '' : 'filter-blue-600'}`} />
+                  <p>{link.label.split(/\s+/)[0]}</p>
+                </Link>
+              );
+            })
+          }
+        </div>   
+      </section>
     </div>
- 
   )
- }
- 
- export default Bottombar;
+}
+
+export default Bottombar;
