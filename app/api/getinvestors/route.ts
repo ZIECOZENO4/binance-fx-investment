1f98fdb5e774c7e448889f8146b4a4dfeac8d453
ZIECOZENO4 <ziecozeno4@gmail.com>
2024-03-25 13:01:36 +0100

    bhhg

--- app/api/getinvestors/route.ts
+++ app/api/getinvestors/route.ts
@@ -6,20 +6,21 @@ export const dynamic = 'force-dynamic';
 
 export async function GET(request: NextRequest) {
     try {
-
         const outInvestments = await prisma.outInvest.findMany({
+            where: {
+                confirmed: true, // Fetch only confirmed investments
+            },
             include: {
-                user: true,
-                confirmed: true,
+                user: true, // Include the related user data
             },
             orderBy: {
-                time: 'desc',
+                time: 'desc', // Order by the time field in descending order
             },
         });
 
         return NextResponse.json(outInvestments);
     } catch (error) {
-        console.error('Error fetching ', error);
-        return NextResponse.json({ error: 'Error fetching' }, { status: 500 });
+        console.error('Error fetching confirmed investments:', error);
+        return NextResponse.json({ error: 'Error fetching confirmed investments' }, { status: 500 });
     }
 }
