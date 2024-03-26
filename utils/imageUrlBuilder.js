41bf74face595a26da1f19ce358d341b2f72701e
ZIECOZENO4 <ziecozeno4@gmail.com>
2024-03-26 08:36:47 +0100

    iboguvyli;jkn;

--- utils/imageUrlBuilder.js
+++
@@ -1,9 +0,0 @@
-
-import {sanityClient} from './sanityclient';
-import imageUrlBuilder from '@sanity/image-url';
-
-const builder = imageUrlBuilder(sanityClient);
-
-export function urlFor(source) {
-  return builder.image(source);
-}
