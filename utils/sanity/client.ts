7a23155cb1059e5179031850199cce891e2227f0
ZIECOZENO4 <ziecozeno4@gmail.com>
2024-03-26 08:54:09 +0100

    kjbiuvf78

---
+++ utils/sanity/client.ts
@@ -0,0 +1,18 @@
+import {createClient} from 'next-sanity';
+import imageUrlBuilder from "@sanity/image-url"
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID 
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET 
+const token = process.env.NEXT_PUBLIC_SANITY_API_TOKEN
+export const client = createClient({
+  projectId,
+  dataset,
+apiVersion: `2022-03-07`,
+token,
+  useCdn: false, 
+})
+
+const builder = imageUrlBuilder(client)
+
+export function urlFor(source: any) {
+  return builder.image(source);
+}
