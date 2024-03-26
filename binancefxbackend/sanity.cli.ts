57308647dbbd61f6a9a9c0597d71dd6cd25f0828
ZIECOZENO4 <ziecozeno4@gmail.com>
2024-03-26 09:00:13 +0100

    kbvuybjlkk

--- binancefxbackend/sanity.cli.ts
+++
@@ -1,8 +0,0 @@
-import {defineCliConfig} from 'sanity/cli'
-
-export default defineCliConfig({
-  api: {
-    projectId: 'h6jp0f96',
-    dataset: 'production'
-  }
-})
--- binancefxbackend/schemaTypes/index.ts
+++
@@ -1,7 +0,0 @@
-import blockContent from './blockContent'
-import category from './category'
-import post from './post'
-import author from './author'
-import testimonial from "./testimonial"
-
-export const schemaTypes = [post, author, category, blockContent, testimonial]
