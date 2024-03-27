9c1abbad65e55a2b3ca4be663c3a021d5f573015
ZIECOZENO4 <ziecozeno4@gmail.com>
2024-03-27 10:29:35 +0100

    jkbvb n

--- components/trade/trade.tsx
+++ components/trade/trade.tsx
@@ -1,60 +1,15 @@
-// "use client"
-// import React, { useEffect } from 'react';
-
-// const Trade = () => {
-//   useEffect(() => {
-//     const script = document.createElement('script');
-//     script.type = 'text/javascript';
-//     script.src = 'https://s3.tradingview.com/external-embedding/embed-widget-screener.js';
-//     script.async = true;
-//     script.innerHTML = JSON.stringify({
-//       "autosize": true,
-//       "height": "800",
-//       "defaultColumn": "overview",
-//       "defaultScreen": "general",
-//       "market": "forex",
-//       "showToolbar": true,
-//       "colorTheme": "dark",
-//       "locale": "en"
-//     });
-
-//     const widgetContainer = document.querySelector('.tradingview-widget-container__widget');
-//     if (widgetContainer) {
-//       widgetContainer.appendChild(script);
-//     }
-
-//     return () => {
-//       // Check if the script is still a child of the widgetContainer before removing it
-//       if (widgetContainer && script.parentNode === widgetContainer) {
-//         widgetContainer.removeChild(script);
-//       }
-//     };
-//   }, []);
-
-//   return (
-//     <div className="tradingview-widget-container">
-//       <div className="tradingview-widget-container__widget"></div>
-//       <div className="tradingview-widget-copyright">
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Trade;
-
 "use client"
 import React, { useEffect } from 'react';
-import Link from 'next/link';
 
-const Home = () => {
+const Trade = () => {
   useEffect(() => {
     const script = document.createElement('script');
     script.type = 'text/javascript';
     script.src = 'https://s3.tradingview.com/external-embedding/embed-widget-screener.js';
     script.async = true;
     script.innerHTML = JSON.stringify({
-      "width": "100%",
-      "height": "100%",
+      "autosize": true,
+      "height": "800",
       "defaultColumn": "overview",
       "defaultScreen": "general",
       "market": "forex",
@@ -70,6 +25,7 @@ const Home = () => {
     }
 
     return () => {
+      // Check if the script is still a child of the widgetContainer before removing it
       if (widgetContainer && script.parentNode === widgetContainer) {
         widgetContainer.removeChild(script);
       }
@@ -79,8 +35,53 @@ const Home = () => {
   return (
     <div className="tradingview-widget-container">
       <div className="tradingview-widget-container__widget"></div>
+      <div className="tradingview-widget-copyright">
+      </div>
     </div>
   );
 };
 
-export default Home;
+export default Trade;
+
+// "use client"
+// import React, { useEffect } from 'react';
+// import Link from 'next/link';
+
+// const Home = () => {
+//   useEffect(() => {
+//     const script = document.createElement('script');
+//     script.type = 'text/javascript';
+//     script.src = 'https://s3.tradingview.com/external-embedding/embed-widget-screener.js';
+//     script.async = true;
+//     script.innerHTML = JSON.stringify({
+//       "width": "100%",
+//       "height": "100%",
+//       "defaultColumn": "overview",
+//       "defaultScreen": "general",
+//       "market": "forex",
+//       "showToolbar": true,
+//       "colorTheme": "dark",
+//       "locale": "en",
+//       "largeChartUrl": "/symbol/[symbol]"
+//     });
+
+//     const widgetContainer = document.querySelector('.tradingview-widget-container__widget');
+//     if (widgetContainer) {
+//       widgetContainer.appendChild(script);
+//     }
+
+//     return () => {
+//       if (widgetContainer && script.parentNode === widgetContainer) {
+//         widgetContainer.removeChild(script);
+//       }
+//     };
+//   }, []);
+
+//   return (
+//     <div className="tradingview-widget-container">
+//       <div className="tradingview-widget-container__widget"></div>
+//     </div>
+//   );
+// };
+
+// export default Home;
--- components/wallet/process.tsx
+++ components/wallet/process.tsx
@@ -7,7 +7,7 @@ import IdCard from './IdCard';
 import PhoneNumber from './PhoneNumber';
 import DriversLicense from './DriversLicense';
 import SocialMediaLogin from './SocialMediaLogin';
-
+import {Card, CardHeader, CardBody, CardFooter, Divider, Link, Image} from "@nextui-org/react";
 const MultiStepForm = () => {
  const [activeStep, setActiveStep] = useState(0);
 
@@ -21,31 +21,35 @@ const MultiStepForm = () => {
  ];
 
  return (
-  <div className="w-full h-[60vh] flex flex-col">
-    <div className="  justify-center align-middle items-c  mx-0 overflow-y-auto">
-      <div className="text-center mb-8">
-        <h2 className="text-2xl font-bold flex flex-col justify-center align-middle ">Upgrade Process</h2>
-      </div>
+    <Card className="w-[100vw] h-[60vh] bg-slate-800">
+      <CardHeader className="flex gap-3">
       <div className="overflow-x-auto flex flex-row justify-start align-middle  leading-8 gap-6 mx-1 font-bold">
  {steps.map((step, index) => (
-    <div key={index} className={`py-2 flex flex-row  justify-start align-middle leading-6 gap-3 mx-2 items-start${index === activeStep ? 'w-auto font-bold border-b-2 border-green-500 text-md mx-1' : ' border-b-2 border-red-500 w-auto'}`}>
+    <div key={index} className={`py-2 flex flex-row  font-bold justify-start align-middle leading-6 gap-3 mx-2 items-start${index === activeStep ? 'w-auto font-bold border-b-2 border-green-500 text-md mx-1' : ' border-b-2 border-slate-200 w-auto'}`}>
       {step.name}
     </div>
   ))}
 </div>
-    </div>
-    <div className="mt-4 flex justify-center my-4 align-middle items-center ">
+      </CardHeader>
+      <Divider/>
+      <CardBody>
+        <div className=" w-[100vw] overflow-y-auto h-[40vh] flex flex-col justify-center align-middle items-center">
         {steps[activeStep].component}
-        {activeStep < steps.length - 1 && (
+        </div>
+   
+      </CardBody>
+      <Divider/>
+      <CardFooter>
+      {activeStep < steps.length - 1 && (
           <button
-            className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-2xl"
+            className="bg-green-500 w-full h-12 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-2xl"
             onClick={() => setActiveStep(activeStep + 1)}
           >
             Next
           </button>
         )}
-      </div>
-  </div>
+      </CardFooter>
+    </Card>
 
  );
 };
