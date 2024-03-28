90a9e2edbbaa0e5d40edfb275844b1f9cd8c0cd3
ZIECOZENO4 <ziecozeno4@gmail.com>
2024-03-28 15:56:52 +0100

    vhbkmjlb k

--- app/wallethistory/page.tsx
+++ app/wallethistory/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React from "react";
 import { useUserPayments } from '@/tenstack-hooks/user-payment';
 
@@ -12,7 +13,6 @@ const PaymentDetails = () => {
                     <p>Amount: {payment.amount}</p>
                     <p>Coin: {payment.coin}</p>
                     <p>Plan: {payment.plan}</p>
-                    {/* Display other payment details */}
                 </div>
             ))}
         </div>
